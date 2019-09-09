abe3e73a4302d16dc99136108e12d09e31e914b9
MarlonLira <marlonlira3@gmail.com>
2019-09-08 22:57:13 -0300

    Criação das Views e Models relacionados ao produto

---
+++ Ruler/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ruler.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        public ActionResult CreateProduct() {
+            return View();
+        }
+
+        public ActionResult CreateProductGroup() {
+            return View();
+        }
+    }
+}
---
+++ Ruler/Models/ProductMdl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Ruler.Models {
+    public class ProductMdl {
+
+        [DisplayName("Nome")]
+        public String Name { get; set; }
+
+        [DisplayName("Quantidade")]
+        public Int32 Amount { get; set; }
+
+        [DisplayName("Tipo")]
+        public String Type { get; set; }
+
+        [DisplayName("Grupo")]
+        public ProductGroupMdl Group { get; set; }
+
+    }
+}
